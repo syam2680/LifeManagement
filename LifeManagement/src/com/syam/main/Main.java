f2eb46d00ef9790cc447a54298514c9ce7877550
Mazhavanheril <syamk@amazon.com>
2017-09-26 13:06:35 +0530

    Testing sql lite

--- LifeManagement/src/com/syam/main/Main.java
+++ LifeManagement/src/com/syam/main/Main.java
@@ -1,14 +1,34 @@
 package com.syam.main;
 
-import com.syam.reporter.SummaryAPI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class Main 
-{
+public class Main {
 	public static void main(String[] args) {
-	     try {	       
-	          System.out.println(SummaryAPI.getAllTasks());
-	     } catch (Exception e) {
-	          e.printStackTrace();
-	     }
+		String url = "jdbc:sqlite:/Users/syamk/Documents/Database/lifemanagement.db";
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC").newInstance();
+			conn = DriverManager.getConnection(url);
+			System.out.println("Connection to SQLite has been established.");
+			String sql = "INSERT INTO GOALS(DESCRIPTION,CATEGORY,START_DATE,DUE_DATE,END_DATE,STATUS,PRIOTIY) "
+					+ " VALUES(?,?,?,?,?,?,?)";
+			 PreparedStatement pstmt = conn.prepareStatement(sql);
+	            pstmt.setString(1, "goal");
+	            pstmt.setString(2, "category");
+	            pstmt.setString(3, "startDate");
+	            pstmt.setString(4, "dueDate");
+	            pstmt.setString(5, "dueDate");
+	            pstmt.setString(6, "YET TO START");
+	            pstmt.setInt(7, 10);
+	            //pstmt.setInt(8, parentGoalID);
+	            int c=pstmt.executeUpdate();
+	            System.out.println(c);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- LifeManagement/src/com/syam/servlets/CreateTask.java
+++ LifeManagement/src/com/syam/servlets/CreateTask.java
@@ -1,10 +1,12 @@
 package com.syam.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,34 +14,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.syam.life.Task;
-
 /**
  * Servlet implementation class CreateTask
  */
 @WebServlet("/CreateTask")
 public class CreateTask extends HttpServlet {
 
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String taskName=request.getParameter("taskName");
+		String goal=request.getParameter("goal");
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm"); 
-		Date startDate=null;
-		Date dueDate=null;
+		String startDate=null;
+		String dueDate=null;
 		int priority=Integer.valueOf(request.getParameter("priority"));
 		String category=request.getParameter("category");
-		try {
-			startDate=df.parse(request.getParameter("startDate"));
-			dueDate=df.parse(request.getParameter("dueDate"));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Task t=new Task(taskName,startDate,dueDate,priority,category);
-		response.sendRedirect("/LifeManagement/GetTask?id="+t.getTaskID());
+		Integer parentGoalID=request.getParameter("parent")==null ? -1 : Integer.valueOf(request.getParameter("parent"));
+		String sql = "INSERT INTO GOALS(DESCRIPTION,CATEGORY,START_DATE,DUE_DATE,END_DATE,STATUS,PRIOTIY,PARENT_GOAL_ID) "
+				+ " VALUES(?,?,?,?,?,?,?,?)";
+		
+			startDate=request.getParameter("startDate");
+			dueDate=request.getParameter("dueDate");
+		
+		try  {
+			
+			String url = "jdbc:sqlite:/Users/syamk/Documents/Database/lifemanagement.db";
+	        Connection conn = null;
+	        try {
+	        	Class.forName("org.sqlite.JDBC").newInstance();
+	            conn = DriverManager.getConnection(url);
+	            System.out.println("Connection to SQLite has been established.");
+	        } catch (Exception e) {
+	            System.out.println(e.getMessage());
+	        }
+	        PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, goal);
+            pstmt.setString(2, category);
+            pstmt.setString(3, startDate);
+            pstmt.setString(4, dueDate);
+            pstmt.setString(5, dueDate);
+            pstmt.setString(6, "YET TO START");
+            pstmt.setInt(7, priority);
+            pstmt.setInt(8, parentGoalID);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 	}
 
 }
